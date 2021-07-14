7c07b1d47465ec69ae0b858991191d8649ef803e
Devon <devonross50@yahoo.com>
2021-07-13 19:15:30 -0500

    updated post layout for basic structure

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,25 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post } = require('../models');
+const { Post, User } = require('../models');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
     console.log(`===========
     get route called
     ==============`)
-    // exports.index = function(request,response,next){
-    //     Post.findAll().then(dbTest => {console.log(dbTest)}).catch(err=>{console.log(err)})    
-    //  }
-    // const data = await Post.findAll()
-    Post.findAll().then(data => {
+    Post.findAll({
+        attributes: [
+          'id',
+          'post_body',
+          'title',
+        ],
+        include: [
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      }).then(data => {
         const posts = data.map(post => post.get({ plain: true }));
         res.render('homepage', {
         posts,
