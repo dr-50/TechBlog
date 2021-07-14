027b9a74f8f4e30341f514bb21be59261745be81
Devon <devonross50@yahoo.com>
2021-07-14 11:41:28 -0500

    updated put post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -25,4 +25,39 @@ router.get('/', (req, res) => {
     })
 })
 
+router.put('/:id', withAuth, (req, res) => {
+    console.log('called')
+    Post.update(
+        {
+            title: req.body.title,
+            post_body: req.body.post_body
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: "No post found with this id"});
+            return;
+        }
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_body: req.body.post_body,
+        user_id: req.session.user_id
+    }).then(dbPostData=> res.json(dbPostData))
+    .catch(err=> {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
 module.exports = router;
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -19,4 +19,26 @@ router.get('/', withAuth, (req, res) => {
     })
 })
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+        where: {
+            user_id: req.session.user_id
+        }
+    })
+    .then(dbPostData => {
+        if (dbPostData) {
+            const post = dbPostData.get({ plain: true });
+    
+            res.render('edit-post', {
+                post, 
+                loggedIn: true
+            });
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 module.exports = router;
